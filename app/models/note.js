9023e86dbc1c788f1e39969bc35d48434e68040b
Lucas <lucas.taferdevs@gmail.com>
2022-02-23 17:20:26 -0300

    first-deploy

--- app/models/note.js
+++ app/models/note.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose');
 
-
-var noteSchema = new mongoose.Schema({
-    title: String,
-    body: String,
-    created_at: {type: Date, default: Date.now},
-    updated_at: {type: Date, default: Date.now},
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    }
+let noteSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 });
 
-noteSchema.index({'title': 'text', 'body': 'text'})
+noteSchema.index({'title': 'text', 'body': 'text'});
 
 module.exports = mongoose.model('Note', noteSchema);
--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/tafernotes', {
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
+mongoose
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true, 
+    useUnifiedTopology: true,
     useCreateIndex: true,
-}).then(() => console.log('Connected succesfull.'))
-.catch((err) => console.log(err));
+  })
+  .then(() => console.log("Connected succesfull."))
+  .catch((err) => console.log(err));
